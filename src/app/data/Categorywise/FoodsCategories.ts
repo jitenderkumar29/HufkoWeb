5c76faf409a2ba5adf86aca6243d6531fef80843
jitenderkumar29 <jitenderkumar2929@gmail.com>
2026-07-26 22:18:25 +0530

    Added All category in Food category

--- src/app/data/Categorywise/FoodsCategories.ts
+++ src/app/data/Categorywise/FoodsCategories.ts
@@ -1,67 +1,735 @@
 import { SubHeaderItem } from "@/components/Header/SubHeader/SubHeader";
 
 export const FoodsCategories = [
+  // {
+  //   name: "All",
+  //   imageUrl: "/products/all_food.png",
+  //   url: "/"
+  // },
   {
-    name: "Masala Bhelpuri",
-    imageUrl: "/products/Bhelpuri.png",
-    url: "/",
+    name: "Diet",
+    imageUrl: "/products/diet_food.png",
+    url: "/"
   },
   {
-    name: "Double Egg Sandwich",
-    imageUrl: "/products/DoubleEggCheeseSandwich.png",
-    url: "/",
+    name: "Pizzas",
+    imageUrl: "/products/pizzas_food.png",
+    url: "/"
   },
   {
-    name: "Tiramisu Order",
-    imageUrl: "/products/Tiramisu.png",
-    url: "/",
+    name: "Cakes",
+    imageUrl: "/products/cakes_food.png",
+    url: "/"
   },
   {
-    name: "Chicken Puff",
-    imageUrl: "/products/ChickenPuff.png",
-    url: "/",
+    name: "Momos",
+    imageUrl: "/products/momos_food.png",
+    url: "/"
   },
   {
-    name: "Veg Puff Order",
-    imageUrl: "/products/VegPuff.png",
-    url: "/",
+    name: "Rolls",
+    imageUrl: "/products/rolls_food.png",
+    url: "/"
   },
   {
-    name: "Choco Walnut",
-    imageUrl: "/products/ChocoWalnutBrownie.png",
-    url: "/",
+    name: "Burgers",
+    imageUrl: "/products/burgers_food.png",
+    url: "/"
+  },
+  {
+    name: "Chole Bhature",
+    imageUrl: "/products/chole_bhature_food.png",
+    url: "/"
+  },
+  {
+    name: "Salad",
+    imageUrl: "/products/salad_food.png",
+    url: "/"
+  },
+  {
+    name: "Patty",
+    imageUrl: "/products/patty_food.png",
+    url: "/"
+  },
+  {
+    name: "Chinese",
+    imageUrl: "/products/chinese_food.png",
+    url: "/"
+  },
+  {
+    name: "Ice Cream",
+    imageUrl: "/products/ice_cream_food.png",
+    url: "/"
+  },
+  {
+    name: "Appam",
+    imageUrl: "/products/appam_food.png",
+    url: "/"
+  },
+  {
+    name: "Bath",
+    imageUrl: "/products/bath_food.png",
+    url: "/"
+  },
+  {
+    name: "Bonda",
+    imageUrl: "/products/bonda_food.png",
+    url: "/"
+  },
+  {
+    name: "Cutlet",
+    imageUrl: "/products/cutlet_food.png",
+    url: "/"
+  },
+  {
+    name: "Dessert",
+    imageUrl: "/products/dessert_food.png",
+    url: "/"
+  },
+  {
+    name: "Dhokla",
+    imageUrl: "/products/dhokla_food.png",
+    url: "/"
+  },
+  {
+    name: "Dosa",
+    imageUrl: "/products/dosa_food.png",
+    url: "/"
+  },
+  {
+    name: "Dholda",
+    imageUrl: "/products/dholda_food.png",
+    url: "/"
+  },
+  {
+    name: "Gulab Jamun",
+    imageUrl: "/products/gulab_jamun_food.png",
+    url: "/"
+  },
+  {
+    name: "Idli",
+    imageUrl: "/products/idli_food.png",
+    url: "/"
+  },
+  {
+    name: "Biryani",
+    imageUrl: "/products/biryani_food.png",
+    url: "/"
+  },
+  {
+    name: "Thali",
+    imageUrl: "/products/thali_food.png",
+    url: "/"
+  },
+  {
+    name: "Chicken",
+    imageUrl: "/products/chicken_food.png",
+    url: "/"
+  },
+  {
+    name: "Veg Meal",
+    imageUrl: "/products/veg_meal_food.png",
+    url: "/"
+  },
+  {
+    name: "North Indian",
+    imageUrl: "/products/north_indian_food.png",
+    url: "/"
+  },
+  {
+    name: "Paneer",
+    imageUrl: "/products/paneer_food.png",
+    url: "/"
+  },
+  {
+    name: "Fried Rice",
+    imageUrl: "/products/fried_rice_food.png",
+    url: "/"
+  },
+  {
+    name: "Noodles",
+    imageUrl: "/products/noodles_food.png",
+    url: "/"
+  },
+  {
+    name: "Paratha",
+    imageUrl: "/products/paratha_food.png",
+    url: "/"
+  },
+  {
+    name: "Shawarma",
+    imageUrl: "/products/shawarma_food.png",
+    url: "/"
+  },
+  {
+    name: "South Indian",
+    imageUrl: "/products/south_indian_food.png",
+    url: "/"
+  },
+  {
+    name: "Aloo Tikki",
+    imageUrl: "/products/aloo_tikki_food.png",
+    url: "/"
+  },
+  {
+    name: "Pasta",
+    imageUrl: "/products/pasta_food.png",
+    url: "/"
+  },
+  {
+    name: "Pastry",
+    imageUrl: "/products/pastry_food.png",
+    url: "/"
+  },
+  {
+    name: "Pav Bhaji",
+    imageUrl: "/products/pav_bhaji_food.png",
+    url: "/"
+  },
+  {
+    name: "Sandwich",
+    imageUrl: "/products/sandwich_food.png",
+    url: "/"
+  },
+  {
+    name: "Shake",
+    imageUrl: "/products/shake_food.png",
+    url: "/"
+  },
+  {
+    name: "Samosa",
+    imageUrl: "/products/samosa_food.png",
+    url: "/"
+  },
+  {
+    name: "Poori",
+    imageUrl: "/products/poori_food.png",
+    url: "/"
+  },
+  {
+    name: "Bowl",
+    imageUrl: "/products/bowl_food.png",
+    url: "/"
+  },
+  {
+    name: "Poha",
+    imageUrl: "/products/poha_food.png",
+    url: "/"
+  },
+  {
+    name: "Sweets",
+    imageUrl: "/products/sweets_food.png",
+    url: "/"
+  },
+  {
+    name: "Chole Poori",
+    imageUrl: "/products/chole_poori_food.png",
+    url: "/"
+  },
+  {
+    name: "Khichdi",
+    imageUrl: "/products/khichdi_food.png",
+    url: "/"
+  },
+  {
+    name: "Chilli Chicken",
+    imageUrl: "/products/chilli_chicken_food.png",
+    url: "/"
+  },
+  {
+    name: "Tea",
+    imageUrl: "/products/tea_food.png",
+    url: "/"
+  },
+  {
+    name: "Vada Pav",
+    imageUrl: "/products/vada_pav_food.png",
+    url: "/"
   },
   {
     name: "Masala Maggi",
-    imageUrl: "/products/ChickenMasalaMaggi.png",
-    url: "/",
+    imageUrl: "/products/masala_maggi_food.png",
+    url: "/"
   },
   {
-    name: "Bull's-eye egg - 2Pcs",
-    imageUrl: "/products/Bullseyeegg2Pcs.png",
-    url: "/",
+    name: "Kulche",
+    imageUrl: "/products/kulche_food.png",
+    url: "/"
   },
   {
-    name: "Plain Curd Matka",
-    imageUrl: "/products/PlainCurd.png",
-    url: "/",
+    name: "Wings",
+    imageUrl: "/products/wings_food.png",
+    url: "/"
   },
   {
-    name: "Mint Mojito Fizzy cooler",
-    imageUrl: "/products/MintMojitoFizzycooler.png",
-    url: "/",
+    name: "Aloo Poori",
+    imageUrl: "/products/aloo_poori_food.png",
+    url: "/"
   },
   {
-    name: "Pepper Maggi",
-    imageUrl: "/products/BlackPepperMaggiwithPeanuts.png",
-    url: "/",
+    name: "Omelette",
+    imageUrl: "/products/omelette_food.png",
+    url: "/"
   },
   {
-    name: "250 ml Cold Coffee",
-    imageUrl: "/products/250mlColdCoffee.png",
-    url: "/",
+    name: "Non Veg Meal",
+    imageUrl: "/products/non_veg_meal_food.png",
+    url: "/"
   },
+  {
+    name: "Bread Pakoda",
+    imageUrl: "/products/bread_pakoda_food.png",
+    url: "/"
+  },
+  {
+    name: "Coffee",
+    imageUrl: "/products/coffee_food.png",
+    url: "/"
+  },
+  {
+    name: "Poori Bhaji",
+    imageUrl: "/products/poori_bhaji_food.png",
+    url: "/"
+  },
+  {
+    name: "Pulao",
+    imageUrl: "/products/pulao_food.png",
+    url: "/"
+  },
+  {
+    name: "Chur Chur Naan",
+    imageUrl: "/products/chur_chur_naan_food.png",
+    url: "/"
+  },
+  {
+    name: "Kebabs",
+    imageUrl: "/products/kebabs_food.png",
+    url: "/"
+  },
+  {
+    name: "Panipuri",
+    imageUrl: "/products/panipuri_food.png",
+    url: "/"
+  },
+  {
+    name: "Rasmalai",
+    imageUrl: "/products/rasmalai_food.png",
+    url: "/"
+  },
+  {
+    name: "Mutton",
+    imageUrl: "/products/mutton_food.png",
+    url: "/"
+  },
+  {
+    name: "Fish",
+    imageUrl: "/products/fish_food.png",
+    url: "/"
+  },
+  {
+    name: "Pakoda",
+    imageUrl: "/products/pakoda_food.png",
+    url: "/"
+  },
+  {
+    name: "Halwa",
+    imageUrl: "/products/halwa_food.png",
+    url: "/"
+  },
+  {
+    name: "Chop Suey",
+    imageUrl: "/products/chop_suey_food.png",
+    url: "/"
+  },
+  {
+    name: "Korma",
+    imageUrl: "/products/korma_food.png",
+    url: "/"
+  },
+  {
+    name: "Namkeen",
+    imageUrl: "/products/namkeen_food.png",
+    url: "/"
+  },
+  {
+    name: "Mushrooms",
+    imageUrl: "/products/mushrooms_food.png",
+    url: "/"
+  },
+  {
+    name: "Keema",
+    imageUrl: "/products/keema_food.png",
+    url: "/"
+  },
+  {
+    name: "Sundae",
+    imageUrl: "/products/sundae_food.png",
+    url: "/"
+  },
+  {
+    name: "Rasgulla",
+    imageUrl: "/products/rasgulla_food.png",
+    url: "/"
+  },
+  {
+    name: "Butter Chicken",
+    imageUrl: "/products/butter_chicken_food.png",
+    url: "/"
+  },
+  {
+    name: "Raj Kachori",
+    imageUrl: "/products/raj_kachori_food.png",
+    url: "/"
+  },
+  {
+    name: "Chaat",
+    imageUrl: "/products/chaat_food.png",
+    url: "/"
+  },
+  {
+    name: "Uttapam",
+    imageUrl: "/products/uttapam_food.png",
+    url: "/"
+  },
+  {
+    name: "Doughnut",
+    imageUrl: "/products/doughnut_food.png",
+    url: "/"
+  },
+  {
+    name: "Juice",
+    imageUrl: "/products/juice_food.png",
+    url: "/"
+  },
+  {
+    name: "Lassi",
+    imageUrl: "/products/lassi_food.png",
+    url: "/"
+  },
+  {
+    name: "Malai Kofta",
+    imageUrl: "/products/malai_kofta_food.png",
+    url: "/"
+  },
+  {
+    name: "Dahi Balle",
+    imageUrl: "/products/dahi_balle_food.png",
+    url: "/"
+  },
+  {
+    name: "Rajma",
+    imageUrl: "/products/rajma_food.png",
+    url: "/"
+  },
+  {
+    name: "Chicken Handi",
+    imageUrl: "/products/chicken_handi_food.png",
+    url: "/"
+  },
+  {
+    name: "Cupcake",
+    imageUrl: "/products/cupcake_food.png",
+    url: "/"
+  },
+  {
+    name: "Bhel",
+    imageUrl: "/products/bhel_food.png",
+    url: "/"
+  },
+  {
+    name: "Muffin",
+    imageUrl: "/products/muffin_food.png",
+    url: "/"
+  },
+  {
+    name: "Cookies",
+    imageUrl: "/products/cookies_food.png",
+    url: "/"
+  },
+  {
+    name: "Chicken Chap",
+    imageUrl: "/products/chicken_cha_food.png",
+    url: "/"
+  },
+  {
+    name: "Paneer Kulche",
+    imageUrl: "/products/paneer_kulche_food.png",
+    url: "/"
+  },
+  {
+    name: "Chaach",
+    imageUrl: "/products/chaach_food.png",
+    url: "/"
+  },
+  {
+    name: "Veg Lollipop",
+    imageUrl: "/products/veg_lollipop_food.png",
+    url: "/"
+  },
+  {
+    name: "Sub",
+    imageUrl: "/products/sub_food.png",
+    url: "/"
+  },
+  {
+    name: "Pancake",
+    imageUrl: "/products/pancake_food.png",
+    url: "/"
+  },
+  {
+    name: "Nihari",
+    imageUrl: "/products/nihari_food.png",
+    url: "/"
+  },
+  {
+    name: "Tacos",
+    imageUrl: "/products/tacos_food.png",
+    url: "/"
+  },
+  {
+    name: "Thepla",
+    imageUrl: "/products/thepla_food.png",
+    url: "/"
+  },
+  {
+    name: "Fafda",
+    imageUrl: "/products/fafda_food.png",
+    url: "/"
+  },
+  {
+    name: "Chocolate",
+    imageUrl: "/products/chocolate_food.png",
+    url: "/"
+  },
+  {
+    name: "Curd Rice",
+    imageUrl: "/products/curd_rice_food.png",
+    url: "/"
+  },
+  {
+    name: "Pudding",
+    imageUrl: "/products/pudding_food.png",
+    url: "/"
+  },
+  {
+    name: "Croissant",
+    imageUrl: "/products/croissant_food.png",
+    url: "/"
+  },
+  {
+    name: "Khandvi",
+    imageUrl: "/products/khandvi_food.png",
+    url: "/"
+  },
+  {
+    name: "Gajak",
+    imageUrl: "/products/gajak_food.png",
+    url: "/"
+  },
+  {
+    name: "Sambar Rice",
+    imageUrl: "/products/sambar_rice_food.png",
+    url: "/"
+  },
+  {
+    name: "Tart",
+    imageUrl: "/products/tart_food.png",
+    url: "/"
+  },
+  {
+    name: "Tiramisu",
+    imageUrl: "/products/tiramisu_food.png",
+    url: "/"
+  },
+  {
+    name: "Pie",
+    imageUrl: "/products/pie_food.png",
+    url: "/"
+  },
+  {
+    name: "Custard",
+    imageUrl: "/products/custard_food.png",
+    url: "/"
+  },
+  {
+    name: "Sev Poori",
+    imageUrl: "/products/sev_poori_food.png",
+    url: "/"
+  },
+  {
+    name: "Mousse",
+    imageUrl: "/products/mousse_food.png",
+    url: "/"
+  },
+  {
+    name: "Dal Kachori",
+    imageUrl: "/products/dal_kachori_food.png",
+    url: "/"
+  },
+  {
+    name: "Jalebi",
+    imageUrl: "/products/jalebi_food.png",
+    url: "/"
+  },
+  {
+    name: "Pyaaj Kachori",
+    imageUrl: "/products/pyaaj_kachori_food.png",
+    url: "/"
+  },
+  {
+    name: "Rajma Rice",
+    imageUrl: "/products/rajma_rice_food.png",
+    url: "/"
+  },
+  {
+    name: "Upma",
+    imageUrl: "/products/upma_food.png",
+    url: "/"
+  },
+  {
+    name: "Manchurian",
+    imageUrl: "/products/manchurian_food.png",
+    url: "/"
+  },
+  {
+    name: "Paneer Pakoda",
+    imageUrl: "/products/paneer_pakoda_food.png",
+    url: "/"
+  },
+  {
+    name: "Cheesecake",
+    imageUrl: "/products/cheesecake_food.png",
+    url: "/"
+  },
+  {
+    name: "Brownie",
+    imageUrl: "/products/brownie_food.png",
+    url: "/"
+  },
+  {
+    name: "Chaap",
+    imageUrl: "/products/chaap_food.png",
+    url: "/"
+  },
+  {
+    name: "Dal",
+    imageUrl: "/products/dal_food.png",
+    url: "/"
+  },
+  {
+    name: "Waffles",
+    imageUrl: "/products/waffles_food.png",
+    url: "/"
+  },
+  {
+    name: "Aloo Kachori",
+    imageUrl: "/products/aloo_kachori_food.png",
+    url: "/"
+  },
+  {
+    name: "Chole Kulche",
+    imageUrl: "/products/chole_kulche_food.png",
+    url: "/"
+  },
+  {
+    name: "Fries",
+    imageUrl: "/products/fries_food.png",
+    url: "/"
+  },
+  {
+    name: "Cold Coffee",
+    imageUrl: "/products/cold_coffee_food.png",
+    url: "/"
+  },
+  {
+    name: "Soup",
+    imageUrl: "/products/soup_food.png",
+    url: "/"
+  },
+  {
+    name: "Bhurji",
+    imageUrl: "/products/bhurji_food.png",
+    url: "/"
+  },
+  {
+    name: "Khasta Kachori",
+    imageUrl: "/products/khasta_kachori_food.png",
+    url: "/"
+  },
+  {
+    name: "Hot Dog",
+    imageUrl: "/products/hot_dog_food.png",
+    url: "/"
+  },
+  // {
+  //   name: "See All",
+  //   imageUrl: "/products/see_all_food.png",
+  //   url: "/"
+  // }
 ];
+
+// export const FoodsCategories = [
+//   {
+//     name: "Masala Bhelpuri",
+//     imageUrl: "/products/Bhelpuri.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Double Egg Sandwich",
+//     imageUrl: "/products/DoubleEggCheeseSandwich.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Tiramisu Order",
+//     imageUrl: "/products/Tiramisu.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Chicken Puff",
+//     imageUrl: "/products/ChickenPuff.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Veg Puff Order",
+//     imageUrl: "/products/VegPuff.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Choco Walnut",
+//     imageUrl: "/products/ChocoWalnutBrownie.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Masala Maggi",
+//     imageUrl: "/products/ChickenMasalaMaggi.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Bull's-eye egg - 2Pcs",
+//     imageUrl: "/products/Bullseyeegg2Pcs.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Plain Curd Matka",
+//     imageUrl: "/products/PlainCurd.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Mint Mojito Fizzy cooler",
+//     imageUrl: "/products/MintMojitoFizzycooler.png",
+//     url: "/",
+//   },
+//   {
+//     name: "Pepper Maggi",
+//     imageUrl: "/products/BlackPepperMaggiwithPeanuts.png",
+//     url: "/",
+//   },
+//   {
+//     name: "250 ml Cold Coffee",
+//     imageUrl: "/products/250mlColdCoffee.png",
+//     url: "/",
+//   },
+// ];
 
 export const foodCategoriesSubHeader: SubHeaderItem[] = [
   { name: "All" },
